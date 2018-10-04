5e59764e273db3cb703913533809cdd743e057d4
Choadis <dahlgrensm@gmail.com>
2018-10-04 09:27:15 -0400

    Lab complete

---
+++ census.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+results = []
+county = []
+pop = []
+
+with open("census.csv") as csvfile:
+    reader = csv.reader(csvfile) #changes contents to floats
+    next(reader, None)
+    for row in reader:
+        county.append(row[2])
+        pop.append(int(row[3]))
+
+plt.bar(county, pop)
+plt.xticks(rotation=45)
+plt.show()
